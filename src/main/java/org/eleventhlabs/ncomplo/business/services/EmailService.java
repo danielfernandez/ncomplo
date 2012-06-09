bd7c3bcff2074382bbe3050f5219e1f2f15af33e
Daniel Fernández <daniel.fernandez@11thlabs.org>
2012-06-09 15:04:47 +0200

    Fixed email service (no op)

--- src/main/java/org/eleventhlabs/ncomplo/business/services/EmailService.java
+++ src/main/java/org/eleventhlabs/ncomplo/business/services/EmailService.java
@@ -15,8 +15,6 @@ public class EmailService {
 
     
     public void sendNewPassword(final String login, final String newPassword) {
-        System.out.println(
-                "The new password for \"" + login + "\" is: \"" + newPassword + "\"");
         // nothing to do yet
     }
     
