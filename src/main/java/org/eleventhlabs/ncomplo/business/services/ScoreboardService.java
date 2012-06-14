2c26c302b84b931c282c92822e24ea4b4b9b5ede
yvangg <yvangg@Pistacho>
2012-06-14 20:03:32 +0200

    Added today game bets in scoreboard page.

--- src/main/java/org/eleventhlabs/ncomplo/business/services/ScoreboardService.java
+++ src/main/java/org/eleventhlabs/ncomplo/business/services/ScoreboardService.java
@@ -1,11 +1,15 @@
 package org.eleventhlabs.ncomplo.business.services;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.time.DateUtils;
 import org.eleventhlabs.ncomplo.business.entities.Bet;
 import org.eleventhlabs.ncomplo.business.entities.League;
 import org.eleventhlabs.ncomplo.business.entities.User;
@@ -48,15 +52,46 @@ public class ScoreboardService {
         
         final League league = this.leagueRepository.findOne(leagueId);
         final Set<User> participants = league.getParticipants();
-        
+        final Calendar calendar = Calendar.getInstance();
+        final Date currentDate = calendar.getTime();
+        String game1SideA = null;
+        Integer score1SideA = null;
+        String game1SideB = null;
+        Integer score1SideB = null;
+        String game2SideA = null;
+        Integer score2SideA = null;
+        String game2SideB = null;
+        Integer score2SideB = null;
         for (final User participant : participants) {
             
             int totalPoints = 0;
             
             final List<Bet> bets = 
                     this.betRepository.findByLeagueIdAndUserLogin(leagueId, participant.getLogin());
-            
+            game1SideA = null;
+            score1SideA = null;
+            game1SideB = null;
+            score1SideB = null;
+            game2SideA = null;
+            score2SideA = null;
+            game2SideB = null;
+            score2SideB = null;
             for (final Bet bet : bets) {
+            	if(DateUtils.isSameDay(currentDate, bet.getGame().getDate())){
+					if (StringUtils.isBlank(game1SideA)
+							&& StringUtils.isBlank(game1SideB)
+							&& score1SideA == null && score1SideB == null) {
+            			game1SideA=bet.getGameSideA().getName(locale);
+            			game1SideB=bet.getGameSideB().getName(locale);
+            			score1SideA=bet.getScoreA();
+            			score1SideB=bet.getScoreB();
+            		} else {
+               			game2SideA=bet.getGameSideA().getName(locale);
+            			game2SideB=bet.getGameSideB().getName(locale);
+            			score2SideA=bet.getScoreA();
+            			score2SideB=bet.getScoreB();
+            		}
+            	}
                 if (bet.getPoints() != null) {
                     if (roundId != null) {
                         if (!bet.getGame().getRound().getId().equals(roundId)) {
@@ -67,8 +102,10 @@ public class ScoreboardService {
                 }
             }
             
-            scoreboard.add(
-                    new ScoreboardEntry(participant,Integer.valueOf(totalPoints)));
+			scoreboard.add(new ScoreboardEntry(participant, Integer
+					.valueOf(totalPoints), game1SideA, game1SideB, game2SideA,
+					game2SideB, score1SideA, score1SideB, score2SideA,
+					score2SideB));
             
         }
         
--- src/main/java/org/eleventhlabs/ncomplo/business/views/ScoreboardEntry.java
+++ src/main/java/org/eleventhlabs/ncomplo/business/views/ScoreboardEntry.java
@@ -13,12 +13,30 @@ public class ScoreboardEntry {
     private Integer position = null;
     private User user;
     private Integer points;
-
+    private String game1SideA;
+    private Integer score1SideA;
+    private String game1SideB;
+    private Integer score1SideB;
+    private String game2SideA;
+    private Integer score2SideA;
+    private String game2SideB;
+    private Integer score2SideB;
     
-    public ScoreboardEntry(final User user, final Integer points) {
+	public ScoreboardEntry(final User user, final Integer points,
+			String game1SideA, String game1SideB, String game2SideA,
+			String game2SideB, Integer score1SideA, Integer score1SideB,
+			Integer score2SideA, Integer score2SideB) {
         super();
         this.user = user;
-        this.points = points;
+		this.points = points;
+		this.game1SideA = game1SideA;
+		this.game1SideB = game1SideB;
+		this.game2SideA = game2SideA;
+		this.game2SideB = game2SideB;
+		this.score1SideA = score1SideA;
+		this.score1SideB = score1SideB;
+		this.score2SideA = score2SideA;
+		this.score2SideB = score2SideB;
     }
 
     
@@ -41,11 +59,106 @@ public class ScoreboardEntry {
     public Integer getPoints() {
         return this.points;
     }
-
-    
     
 
-    public static class ScoreboardEntryComparator implements Comparator<ScoreboardEntry> {
+    public String getGame1SideA() {
+		return game1SideA;
+	}
+
+
+
+	public void setGame1SideA(String game1SideA) {
+		this.game1SideA = game1SideA;
+	}
+
+
+
+	public Integer getScore1SideA() {
+		return score1SideA;
+	}
+
+
+
+	public void setScore1SideA(Integer score1SideA) {
+		this.score1SideA = score1SideA;
+	}
+
+
+
+	public String getGame1SideB() {
+		return game1SideB;
+	}
+
+
+
+	public void setGame1SideB(String game1SideB) {
+		this.game1SideB = game1SideB;
+	}
+
+
+
+	public Integer getScore1SideB() {
+		return score1SideB;
+	}
+
+
+
+	public void setScore1SideB(Integer score1SideB) {
+		this.score1SideB = score1SideB;
+	}
+
+
+
+	public String getGame2SideA() {
+		return game2SideA;
+	}
+
+
+
+	public void setGame2SideA(String game2SideA) {
+		this.game2SideA = game2SideA;
+	}
+
+
+
+	public Integer getScore2SideA() {
+		return score2SideA;
+	}
+
+
+
+	public void setScore2SideA(Integer score2SideA) {
+		this.score2SideA = score2SideA;
+	}
+
+
+
+	public String getGame2SideB() {
+		return game2SideB;
+	}
+
+
+
+	public void setGame2SideB(String game2SideB) {
+		this.game2SideB = game2SideB;
+	}
+
+
+
+	public Integer getScore2SideB() {
+		return score2SideB;
+	}
+
+
+
+	public void setScore2SideB(Integer score2SideB) {
+		this.score2SideB = score2SideB;
+	}
+
+
+
+
+	public static class ScoreboardEntryComparator implements Comparator<ScoreboardEntry> {
         
         private final Locale locale;
         
